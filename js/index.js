2cd6cc0b707ea8c2284f7b41cf139c91d45227e2
jeffhopper <jeffhopperr@gmail.com>
2016-07-06 15:12:53 -0400

    Update 4
    add contact, goods, gallery pages. various style and js updates

--- js/index.js
+++ js/index.js
@@ -1,11 +1,36 @@
-// NAV BUTTON ROTATE
 jQuery(document).ready(function($) {
     $('#plus').click(function() {
+        // NAV BUTTON ROTATE
         $('#plus').toggleClass('rotate');
         // SHOW MENU
         $('nav').toggleClass('show');
-        // CHANGE LOGO COLOR
-        // $('#heroLogo img#pink').toggleClass('show');
     });
+
+    $('#scroll').click(function() {
+        var center = $('#center');
+        $('html,body').animate({ scrollTop: center.offset().top }, 600);
+    });
+    // HIDE SCROLL BUTTON ON SCROLL
+    $(window).scroll(function() {
+        if ($(document).scrollTop() > 100) {
+            $('#scrollButton').addClass('hide');
+
+        } else {
+            $('#scrollButton').removeClass('hide');
+        }
+    });
+    $(window).scroll(function() {
+        if ($(document).scrollTop() > 500) {
+            // HIDE MENU / RESET BUTTON IF VISIBLE ON SCROLL
+            $('nav').removeClass('show');
+            $('#plus').removeClass('rotate');
+        }
+    });
+    $('#footerLogo').click(function() {
+        $('html, body').animate({ scrollTop: 0 }, 500);
+    });
+
+
+
 
 });
